cf8b311ae71ff6f72d54068a28bf9908337766e5
andersback <anders.back@me.com>
2017-05-24 12:46:42 +0200

    post with params work

--- handler.js
+++ handler.js
@@ -1,17 +1,13 @@
 'use strict';
 var pusher = require('./pusher')
 module.exports.push = (event, context, callback) => {
-  pusher.trigger('foo', 'foo', {foo: 'foo'})
+  const requestBody = JSON.parse(event.body)
+  var {channelName, eventName, data} = requestBody
+  pusher.trigger(channelName, eventName, data)
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: JSON.stringify(JSON.stringify(requestBody))
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
